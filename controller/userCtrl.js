1aa5c82e3da2dc4c4fb28e33731a707e0e06ca6e
Zamin <Muhammad.Zamin@pratian.com>
2024-05-03 23:35:12 +0530

    add update password functionilty in userCrtl

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -181,4 +181,19 @@ const unblockUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser, blockUser, unblockUser, handelRefreshToekn, logout }
+const updatePassword = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { password } = req.body;
+    validateMongoDbId(_id);
+    const user = await User.findById(_id);
+    if (password) {
+        user.password = password;
+        const updatedPassword = await user.save();
+        res.json(updatedPassword);
+        console.log(res.json(updatedPassword))
+    } else {
+        res.json(user);
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser, blockUser, unblockUser, handelRefreshToekn, logout, updatePassword }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser, unblockUser, blockUser, handelRefreshToekn, logout } = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser, unblockUser, blockUser, handelRefreshToekn, logout, updatePassword } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlerwares/authMiddleware');
 const router = express.Router();
-router.post('/register', createUser)
-router.post('/login', loginUserCtrl)
-router.get('/all-users', getallUser)
-router.get('/refresh', handelRefreshToekn)
-router.get('/logout', logout)
-router.get('/:id', authMiddleware, isAdmin, getaUser)
-router.delete('/:id', deleteaUser)
-router.put('/edit-user', authMiddleware, updatedUser)
-router.put('/block-user/:id', authMiddleware, isAdmin, blockUser)
-router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser)
+router.post('/register', createUser);
+router.put('/password', authMiddleware, isAdmin, updatePassword);
+router.post('/login', loginUserCtrl);
+router.get('/all-users', getallUser);
+router.get('/refresh', handelRefreshToekn);
+router.get('/logout', logout);
+router.get('/:id', authMiddleware, isAdmin, getaUser);
+router.delete('/:id', deleteaUser);
+router.put('/edit-user', authMiddleware, updatedUser);
+router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
+router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
 module.exports = router;
